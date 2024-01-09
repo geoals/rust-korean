baa63c04a7e17aef9e0271c10f6a566fe9574257
Geir Olav Alsvik <goa95@protonmail.com>
2024-01-09 23:41:36 +0100

    underline all korean words on page load

--- src/content.ts
+++ src/content.ts
@@ -1,97 +1,162 @@
 import { sendToBackground } from "@plasmohq/messaging";
 import type { AnalyzeResponse } from "~background/messages/analyze";
 
-const sendAnalyzeRequestToBG = async (text: string) => {
-  const result = (await sendToBackground({
-    name: "analyze",
-    body: text,
-  })) as { message: AnalyzeResponse };
-
-  return result.message;
+const hangulRegex = /[\uAC00-\uD7AF]/;
+const underlineColor = {
+  seen: "#FACB6E",
+  known: "transparent",
+  unknown: "#F38181",
+  unmatched: "gray",
 };
-
 let analysisResults = {};
 
-// TODO clean up this abomination
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.type === "childList") {
-      mutation.addedNodes.forEach(async (addedNode) => {
-        if (
-          addedNode.nodeType === Node.TEXT_NODE &&
-          addedNode.textContent.trim().length > 0
-        ) {
-          const trimmedText = addedNode.textContent
-            .trim()
-            .replace(/[\(\)\[\]\?\!,\.'"]/g, "");
-          let result = await sendAnalyzeRequestToBG(trimmedText);
+(async () => {
+  const sendAnalyzeRequestToBackground = async (text: string) => {
+    const result = (await sendToBackground({
+      name: "analyze",
+      body: text,
+    })) as { message: AnalyzeResponse };
+
+    return result.message;
+  };
+
+  // TODO clean up this abomination
+  function mutationObserverCallback(mutations: MutationRecord[]) {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        mutation.addedNodes.forEach(async (addedNode) => {
+          if (
+            !(mutation.target instanceof HTMLElement) ||
+            addedNode.nodeType !== Node.TEXT_NODE ||
+            addedNode.textContent === null ||
+            addedNode.textContent.trim().length === 0
+          ) {
+            return;
+          }
+
+          let result = await sendAnalyzeRequestToBackground(
+            addedNode.textContent,
+          );
           analysisResults = { ...analysisResults, ...result };
 
           let newContent = "";
 
           addedNode.textContent.split("\n").forEach((line) => {
             line.split(" ").map((word) => {
-              // Only words contain hangul characters are underlined and reinserted into the DOM
-              const hangulRegex = /[\uAC00-\uD7AF]/;
               if (hangulRegex.test(word)) {
-                const trimmedWord = word
-                  .trim()
-                  .replace(/[\(\)\[\]\?\!,\.'"-]/g, "");
-                const status = getWordStatus(trimmedWord, analysisResults);
-                const underlinecolor = {
-                  seen: "#FACB6E",
-                  known: "transparent",
-                  unknown: "#F38181",
-                  unmatched: "gray",
-                }[status];
-
-                newContent += `<span class="rust-korean ${status}" style="text-decoration: underline 4px ${underlinecolor}; text-underline-offset: 5px;">${word}</span> `;
+                let { hangulWord } = getConsecutiveHangulSubstring(word);
+                const status = getWordStatus(hangulWord, analysisResults);
+                const color = underlineColor[status];
+                newContent += `<span class="rust-korean ${status}" style="text-decoration: underline 0.2rem ${color}; text-underline-offset: 0.3rem;">${word}</span> `;
               }
             });
             newContent += "\n";
           });
 
-          // Replace the content of the target node
           mutation.target.innerHTML = newContent;
-          reapplyUnderlineColors();
+        });
+      }
+    });
+  }
+
+  function getWordStatus(word: string, wordStatuses: AnalyzeResponse) {
+    if (wordStatuses[word] === undefined) {
+      return "unknown";
+    }
+    if (wordStatuses[word].length === 0) {
+      return "unmatched";
+    }
+    return wordStatuses[word][0].status;
+  }
+
+  function getTextNodes(node: Node) {
+    let textNodes: Array<Node> = [];
+
+    if (node.nodeType === Node.TEXT_NODE) {
+      console.log(node.textContent);
+      textNodes.push(node);
+    } else {
+      // Recursively traverse child nodes
+      const children = node.childNodes;
+      for (let i = 0; i < children.length; i++) {
+        textNodes = textNodes.concat(getTextNodes(children[i]));
+      }
+    }
+
+    return textNodes;
+  }
+
+  function underlineHangulWords(textNode: Node) {
+    if (
+      textNode.nodeValue === null ||
+      !(textNode.parentNode instanceof HTMLElement)
+    ) {
+      return;
+    }
+
+    let newHTML = "";
+
+    textNode.nodeValue.split("\n").forEach((line) => {
+      line.split(" ").forEach((word, index) => {
+        if (!hangulRegex.test(word)) {
+          newHTML += word;
+        } else {
+          const {
+            hangulWord,
+            start: startIndex,
+            end: endIndex,
+          } = getConsecutiveHangulSubstring(word);
+          const wordStart = word.substring(0, startIndex);
+          const wordEnd = word.substring(endIndex);
+          const status = getWordStatus(hangulWord, analysisResults);
+          const color = underlineColor[status];
+          newHTML += `${wordStart}<span class="rust-korean" style="text-decoration: underline 0.2rem ${color}; text-underline-offset: 0.3rem;">${hangulWord}</span>${wordEnd}`;
+        }
+        if (index < line.split(" ").length - 1) {
+          newHTML += " ";
         }
       });
+      newHTML += "\n";
+    });
+
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = newHTML;
+    textNode.parentNode.replaceChild(newSpan, textNode);
+  }
+
+  let result = await sendAnalyzeRequestToBackground(document.body.innerText);
+  analysisResults = { ...analysisResults, ...result };
+
+  const allTextNodes = getTextNodes(document.body);
+
+  for (const node of allTextNodes) {
+    if (!hangulRegex.test(node.textContent ?? "")) {
+      continue;
     }
-  });
-});
-
-// reapply underline color based on word status from analysisResults for all words in the document with class "rust-korean"
-// TODO kinda works but analysisResults is not updated when the user changes the status of a word in the popup
-function reapplyUnderlineColors() {
-  const rustKoreanElements = document.getElementsByClassName("rust-korean");
-  for (let i = 0; i < rustKoreanElements.length; i++) {
-    const element = rustKoreanElements[i];
-    const trimmedWord = element.textContent
-      .trim()
-      .replace(/[\(\)\[\]\?\!,\.'"-]/g, "");
-    const status = getWordStatus(trimmedWord, analysisResults);
-    const underlinecolor = {
-      seen: "#FACB6E",
-      known: "transparent",
-      unknown: "#F38181",
-      unmatched: "gray",
-    }[status];
-    element.style.textDecoration = `underline 4px ${underlinecolor}`;
+    underlineHangulWords(node);
   }
+
+  const observer = new MutationObserver(mutationObserverCallback);
+  observer.observe(document.body, { childList: true, subtree: true });
+  // To stop:
+  // observer.disconnect();
+  // make this a toggleswitch in popup.tsx
+})();
+
+function getConsecutiveHangulSubstring(word: string) {
+  let start = 0;
+  let end = word.length;
+  for (let i = 0; i < word.length; i++) {
+    if (hangulRegex.test(word[i])) {
+      start = i;
+      break;
+    }
+  }
+  for (let i = start; i < word.length; i++) {
+    if (!hangulRegex.test(word[i])) {
+      end = i;
+      break;
+    }
+  }
+  return { hangulWord: word.substring(start, end), start, end };
 }
-
-function getWordStatus(word: string, wordStatuses: AnalyzeResponse) {
-  if (wordStatuses[word] === undefined) {
-    return "unknown";
-  }
-  if (wordStatuses[word].length === 0) {
-    return "unmatched";
-  }
-  return wordStatuses[word][0].status;
-}
-
-const targetNode = document.body;
-observer.observe(targetNode, { childList: true, subtree: true });
-// To stop:
-// observer.disconnect();
-// make this a toggleswitch in popup.tsx
